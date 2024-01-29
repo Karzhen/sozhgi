232e3eafaaec221e1f0f1f57979185cd6d6b91d1
Egor Cherkashin <egor9298@mail.ru>
2024-01-30 00:22:38 +0300

    feat: first version

---
+++ scripts/mainLogic.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const backgroundVideo = document.querySelector('.background-video'),
+        main = document.querySelector('main');
+
+    setTimeout(function() {
+        const button = document.createElement('a');
+        button.textContent = 'Перейти к товарам на маркетплейсе';
+        button.href = 'https://ozon.ru/t/dEgNNnb';
+        button.target = '_blank';
+
+        const overlay = document.createElement('div');
+        overlay.classList.add('overlay');
+
+        overlay.style.opacity = '0';
+        button.style.opacity = '0';
+
+        main.appendChild(overlay);
+        main.appendChild(button);
+
+        button.classList.add('modal-button');
+
+        setTimeout(function() {
+            overlay.style.opacity = '1';
+            button.style.opacity = '1';
+        }, 2000); // небольшая задержка
+    }, 2000);
+});
